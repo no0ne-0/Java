7d315b1a8cef155677a752b4a40660f17b2a21ab
Sahil <sahil@Sahils-MacBook-Air.local>
2025-06-01 11:59:55 +0530

    factorial

---
+++ Practice/Factorial.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class Factorial {
+
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = n; i >= 2; i--) {
+            fact = fact * i;
+        }
+        return fact;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.print("factorial of given number is: ");
+        System.out.print(factorial(5));
+    }
+}
---
+++ Practice/Practice.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Practice {
+
+    public static int product(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int number = product(10, 2);
+        System.out.println(number);
+
+    }
+}
